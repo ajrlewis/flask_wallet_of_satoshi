1b66753d834d6ba0e5dd105da6654531f6991f98
ajrlewis <git@ajrlewis.com>
2023-08-08 15:45:55 +0100

    Documented and refactored each method.

--- flask_wallet_of_satoshi/flask_wallet_of_satoshi.py
+++ flask_wallet_of_satoshi/flask_wallet_of_satoshi.py
@@ -1,9 +1,19 @@
-from flask import jsonify
+from flask import jsonify, request
 from wallet_of_satoshi import WalletOfSatoshi as _WalletOfSatoshi
 
 
 class WalletOfSatoshi:
+    """
+    Wrapper class for interacting with Wallet of Satoshi API.
+    """
+
     def __init__(self, app=None):
+        """
+        Initializes the WalletOfSatoshi object.
+
+        Args:
+            app: Flask application object (optional).
+        """
         self.app = app
         self.wallet = None
 
@@ -11,19 +21,70 @@ class WalletOfSatoshi:
             self.init_app(app)
 
     def init_app(self, app):
-        app.route("/.well-known/lnurl/pay")(self.lnurl_pay)
+        """
+        Initializes the Flask application object.
+
+        Args:
+            app: Flask application object.
+        """
+        self.app = app
+        app.route("/.well-known/lnurlp/pay")(self.lnurl_pay)
+        app.route("/lightning/payment-request")(self.payment_request)
 
         @app.errorhandler(500)
         def handle_error(error):
+            """
+            Error handler for 500 Internal Server Error.
+
+            Args:
+                error: The error object.
+
+            Returns:
+                A JSON response with the error message and status code 500.
+            """
             return jsonify({"error": "Wallet of Satoshi username not configured."}), 500
 
-    def lnurl_pay(self):
+        self.set_wallet()
+
+    def set_wallet(self):
+        """
+        Sets up the Wallet of Satoshi instance.
+
+        Returns:
+            A JSON response with the error message and status code 500 if the username is not configured.
+        """
         username = self.app.config.get("WOS_USERNAME")
         if not username:
-            return "", 500
+            return jsonify({"error": "Wallet of Satoshi username not configured."}), 500
 
+        self.wallet = _WalletOfSatoshi(username)
+
+    def lnurl_pay(self):
+        """
+        Handles the lnurlp/pay route.
+
+        Returns:
+            A JSON response with the lnurlp data.
+        """
         if not self.wallet:
-            self.wallet = _WalletOfSatoshi(username)
+            return jsonify({"error": "Wallet not initialized."}), 500
 
         lnurlp = self.wallet.well_known()
         return jsonify(lnurlp)
+
+    def payment_request(self):
+        """
+        Handles the lightning/payment-request route.
+
+        Returns:
+            The payment request.
+        """
+        if not self.wallet:
+            return jsonify({"error": "Wallet not initialized."}), 500
+
+        amount = request.args.get("amount")
+        if not amount:
+            return jsonify({"error": "Amount parameter in mSatoshis is missing."}), 400
+
+        pr = self.wallet.payment_request(amount=amount)
+        return pr
